d05cbf962c8b377f0dd2ea90b2d2a8125e20c608
Frank <andreass.frankk@gmail.com>
2022-10-18 13:02:09 +0300

    edited

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,34 +1,9 @@
 import { Actor, HttpAgent } from '@dfinity/agent';
 import phonebookIdl from '../idls/phonebook.did';
 
-// ---------  data
-
-// {
-//     gateway_canister: null or Principal("ngrpb-5qaaa-aaaaj-adz7a-cai");
-//     token_id: null or "baycdev";
-//     library_id: null or "bayc-1";
-//     app_id: "collection" or Principal("ngrpb-5qaaa-aaaaj-adz7a-cai"); //there should aloways be at least one app_id(I think)
-//     app_command: [
-//       {command: "token"; alias: "-"; value: "bayc-1"};
-//       {command: "library"; alias: "-"; value: "custom_app.html"};
-//     ]
-//     domain: "exos.origyn.network";  //or "exos.surf" or "canister_id.raw.ic0.app"
-//     exos_path: "/-/baycdev/-/bayc-1/-/custom_app.html";
-//     canister_path: "-/bayc-1/-/custom_app.html";"
-//     is_exos: true; //is the current url at the exos level
-//     is_canister: false; //is the current url at the canister level or not
-//     raw: "https://exos.origyn.network/-/baycdev/-/bayc-1/-/custom_app.html"
-//   }
-
-//-----------------------------------
-
-// canister = ids[2]
-// collection = ids[3]
-// token_id = ids[4]
-
 const domainId = window.location.hostname;
 const pathId = window.location.pathname;
-const rawId = window.location;
+const rawId = window.location.href;
 
 const canisterId = () => {
   const ids = pathId.split('/');
@@ -65,7 +40,7 @@ const tokenId = () => {
 
 const isExos = () => {
   let exosId = false;
-  if (pathId.includes('exos')) {
+  if (domainId.includes('exos')) {
     return (exosId = true);
   }
   return exosId;
@@ -73,7 +48,7 @@ const isExos = () => {
 
 const isCanister = () => {
   let canId = false;
-  if (pathId.includes('canister_id')) {
+  if (domainId.includes('canister_id')) {
     return (canId = true);
   }
   return canId;
@@ -90,26 +65,19 @@ const isCanister = () => {
 // };
 
 export function contextModule() {
-  canisterId();
-  appId();
-  collectionId();
-  tokenId();
-  isExos();
-  isCanister();
-
   return {
     gateway_canister: 'null or Principal(ngrpb-5qaaa-aaaaj-adz7a-cai)', // need clarity
-    token_id: tokenId,
-    library_id: canisterId,
-    app_id: collectionId,
+    token_id: tokenId(),
+    library_id: canisterId(),
+    app_id: collectionId(),
     app_command: [
-      { command: 'token', alias: '-', value: tokenId },
-      { command: 'library', alias: '-', value: appId },
+      { command: 'token', alias: '-', value: tokenId() },
+      { command: 'library', alias: '-', value: appId() },
     ],
     domain: domainId,
     exos_path: pathId,
-    is_exos: isExos,
-    is_canister: isCanister,
+    is_exos: isExos(),
+    is_canister: isCanister(),
     raw: rawId,
   };
 }
